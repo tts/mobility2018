05c162ffbd2f5eba20366f22b7bc9e27ab76b66c
Tuija Sonkkila <tts@users.noreply.github.com>
2019-02-11 12:35:54 +0200

    Add files via upload

---
+++ report.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readxl)
+
+# ACRIS report 'Mobility activity last year' exported as an Excel file
+rawdata <- read_excel("Mobility_activities_last_year-11_02_2019.xls")
+
+data <- rawdata %>%
+  rename(start = `Period-4`,
+         end = `End date-5`,
+         person = `Source-ID-1`,
+         type = `Type-2`) %>% 
+  mutate(start = as.Date(start, "%d/%m/%Y"),
+         end = as.Date(end, "%d/%m/%Y")) %>% 
+  select(person, start, end, type)
+
+mobility <- data %>% 
+  mutate(days = end - start) %>% 
+  filter(days >= 30) %>% 
+  group_by(type) %>% 
+  summarize(count = n())
+
